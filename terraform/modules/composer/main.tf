73ac5866e0f0e05037ebd9d15f3894ea4dd8f03e
vali1610 <90977859+vali1610@users.noreply.github.com>
2025-07-19 16:57:30 +0300

    minor fix

--- terraform/modules/composer/main.tf
+++ terraform/modules/composer/main.tf
@@ -2,7 +2,6 @@ resource "google_composer_environment" "this" {
   name   = var.name
   region = var.region
   config {
-    node_count = var.node_count
     software_config {
       image_version = var.image_version
     }
