9dd69375d40ed5df1141eea653a064d8e663929d
vali1610 <90977859+vali1610@users.noreply.github.com>
2025-07-19 19:48:32 +0300

    Improvements

--- terraform/modules/composer/variables.tf
+++ terraform/modules/composer/variables.tf
@@ -2,3 +2,7 @@ variable "name"          { type = string }
 variable "region"        { type = string }
 variable "image_version" { type = string }
 variable "project_id"    { type = string }
+variable "composer_sa_email" {
+  description = "Email of the service account used for Composer"
+  type        = string
+}
