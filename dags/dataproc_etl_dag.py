a79e6f3f0b672c91a597999c8b92baf847d16a44
vali1610 <90977859+vali1610@users.noreply.github.com>
2025-07-24 19:59:57 +0300

    Update dataproc_etl_dag.py

--- dags/dataproc_etl_dag.py
+++ dags/dataproc_etl_dag.py
@@ -2,25 +2,25 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+default_args = {
+    'start_date': datetime(2025, 7, 24),
+}
+
 with DAG(
-    dag_id='run_spark_etl_fallback',
-    start_date=datetime(2025, 7, 24),
+    dag_id='dataproc_etl_direct',
     schedule_interval=None,
+    default_args=default_args,
     catchup=False,
-    tags=['spark', 'dataproc', 'fallback']
 ) as dag:
 
-    submit_spark_job = BashOperator(
+    run_spark_job = BashOperator(
         task_id='submit_spark_job_direct',
-        bash_command='''
+        bash_command="""
+        gcloud auth activate-service-account --key-file=/home/airflow/gcs/data/sa-dataops.json &&
         gcloud dataproc jobs submit pyspark gs://vale-dataops-bucket/jobs/pyspark_job.py \
-            --cluster=vale-dataproc \
-            --region=europe-west1 \
-            --jars=gs://vale-dataops-bucket/libs/delta-spark_2.12-3.2.0.jar,\
-                   gs://vale-dataops-bucket/libs/delta-storage-3.0.0.jar,\
-                   gs://vale-dataops-bucket/libs/hudi-spark3.5-bundle_2.12-1.0.2.jar,\
-                   gs://vale-dataops-bucket/libs/iceberg-spark-runtime-3.5_2.12-1.5.0.jar \
-            -- gs://vale-dataops-bucket/data/customer_data_dirty.csv \
-               gs://vale-dataops-bucket/data/payment_data_dirty.csv
-        '''
+          --cluster=vale-dataproc \
+          --region=europe-west1 \
+          --jars=gs://vale-dataops-bucket/libs/delta-spark_2.12-3.2.0.jar,gs://vale-dataops-bucket/libs/delta-storage-3.0.0.jar,gs://vale-dataops-bucket/libs/hudi-spark3.5-bundle_2.12-1.0.2.jar,gs://vale-dataops-bucket/libs/iceberg-spark-runtime-3.5_2.12-1.5.0.jar \
+          -- gs://vale-dataops-bucket/data/customer_data_dirty.csv gs://vale-dataops-bucket/data/payment_data_dirty.csv
+        """,
     )
