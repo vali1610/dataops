797c3bec38f5f32dc2dceefeba4a91e0293cf0e6
vali1610 <90977859+vali1610@users.noreply.github.com>
2025-07-25 07:20:40 +0300

    Update dataproc_etl_dag.py

--- dags/dataproc_etl_dag.py
+++ dags/dataproc_etl_dag.py
@@ -16,11 +16,10 @@ with DAG(
     run_spark_job = BashOperator(
         task_id='submit_spark_job_direct',
         bash_command="""
-        gcloud auth activate-service-account --key-file=/home/airflow/gcs/data/sa-dataops.json &&
-        gcloud dataproc jobs submit pyspark gs://vale-dataops-bucket/jobs/pyspark_job.py \
-          --cluster=vale-dataproc \
-          --region=europe-west1 \
-          --jars=gs://vale-dataops-bucket/libs/delta-spark_2.12-3.2.0.jar,gs://vale-dataops-bucket/libs/delta-storage-3.0.0.jar,gs://vale-dataops-bucket/libs/hudi-spark3.5-bundle_2.12-1.0.2.jar,gs://vale-dataops-bucket/libs/iceberg-spark-runtime-3.5_2.12-1.5.0.jar \
-          -- gs://vale-dataops-bucket/data/customer_data_dirty.csv gs://vale-dataops-bucket/data/payment_data_dirty.csv
+            gcloud dataproc jobs submit pyspark gs://vale-dataops-bucket/jobs/pyspark_job.py \
+            --cluster=vale-dataproc \
+            --region=europe-west1 \
+            --jars=gs://vale-dataops-bucket/libs/delta-spark_2.12-3.2.0.jar,gs://vale-dataops-bucket/libs/delta-storage-3.0.0.jar,gs://vale-dataops-bucket/libs/hudi-spark3.5-bundle_2.12-1.0.2.jar,gs://vale-dataops-bucket/libs/iceberg-spark-runtime-3.5_2.12-1.5.0.jar \
+            -- gs://vale-dataops-bucket/data/customer_data_dirty.csv gs://vale-dataops-bucket/data/payment_data_dirty.csv
         """,
     )
