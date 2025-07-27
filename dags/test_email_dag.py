57c732f9ab8f1cc45a6e92afaf22af63fe968c35
vali1610 <90977859+vali1610@users.noreply.github.com>
2025-07-27 12:09:14 +0300

    add slack integration

--- dags/test_email_dag.py
+++ dags/test_email_dag.py
@@ -1,28 +1,33 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
-import requests
 from airflow.hooks.base import BaseHook
+import requests
+from datetime import datetime
 
 def notify_slack():
+    # Obține conexiunea definită în Airflow
     conn = BaseHook.get_connection("slack_webhook")
-    url = conn.host + conn.extra_dejson.get("endpoint")
+    webhook_url = conn.host + conn.extra_dejson.get("endpoint")
+    
+    # Mesajul trimis pe Slack
     message = {
-        "text": "✅ Slack alert din DAG Airflow – Vale a reușit! 🚀"
+        "text": "✅ Test Slack Webhook din DAG Airflow! Totul funcționează! 🚀"
     }
-    response = requests.post(url, json=message)
+
+    response = requests.post(webhook_url, json=message)
+
     if response.status_code != 200:
-        raise ValueError(f"Slack request failed: {response.status_code}, {response.text}")
+        raise ValueError(f"Request to Slack failed: {response.status_code}, {response.text}")
 
 with DAG(
-    dag_id='test_slack_alerting',
+    dag_id="slack_notify_dag",
     start_date=datetime(2023, 1, 1),
     schedule_interval=None,
     catchup=False,
-    tags=['test']
+    tags=["slack", "notification"]
 ) as dag:
-    
-    alert = PythonOperator(
-        task_id='notify_slack',
+
+    notify_slack_task = PythonOperator(
+        task_id="notify_slack",
         python_callable=notify_slack
     )
